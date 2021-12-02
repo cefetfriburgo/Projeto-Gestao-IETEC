5f720bf38b5ca343a406f4306974720b7a589156
Bruno Policarpo <9388621+bptfreitas@users.noreply.github.com>
2021-12-02 15:16:15 -0300

    Adicionado os arquivos para construir as bases de dados de desenvolvimento.
    Elas se encontram na raiz do projeto:
    
    src/base_de_dados.sql: constrói toda a estrutura das bases de dados
    src/dados-dev.sql: adiciona permissões, grupos "Administrador" e "Dono da empresa", e as permissões desses grupos

---
+++ src/dados-dev.sql
@@ -0,0 +1,45 @@
+USE mydb;
+
+
+/*
+Rodar o script 'base_de_dados.sql' antes de inserir os dados de teste
+*/
+
+-- -------------------------------------------------------
+-- PERMISSOES POSSIVEIS DO SISTEMA
+-- -------------------------------------------------------
+
+-- Tabela 'Permissao'
+INSERT INTO Permissao VALUES ( 1 , 101 ); -- "table_empresa_write"
+INSERT INTO Permissao VALUES ( 2 , 102 ); -- "table_empresa_read"
+
+
+-- -------------------------------------------------------
+-- GRUPOS DO SISTEMA
+-- -------------------------------------------------------
+
+INSERT INTO Grupo VALUES ( 1, "Administrador" );
+INSERT INTO Grupo VALUES ( 2, "Dono da empresa" );
+
+
+-- -------------------------------------------------------
+-- PERMISSOES DOS GRUPOS 
+-- -------------------------------------------------------
+
+/*
+Grupo "Administrador" pode fazer tudo no sistema
+*/
+INSERT INTO Grupo_has_Permissao VALUES ( 1 , 1 );
+INSERT INTO Grupo_has_Permissao VALUES ( 1 , 2 );
+
+/*
+Grupo "Dono da empresa" pode ler dados da empresa
+*/
+INSERT INTO Grupo_has_Permissao VALUES ( 2 , 2 );
+
+
+-- -------------------------------------------------------
+-- USUARIOS
+-- -------------------------------------------------------
+INSERT INTO Usuário VALUES (  1 , 1, "marcelo.testando@fabrica.org.br", '123');
+
