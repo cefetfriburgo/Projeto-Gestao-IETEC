153538d7c8df6503655af8e0c6e984db46a8d666
0z <rodrigonwl.a@gmail.com>
2021-12-08 15:45:45 -0300

    organizacao dos scripts 08/12

---
+++ src/scripts/alternacao-campos-cadastro-empreendimento.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#cpf_input").prop("disabled", true);
+    $("#cnpj_input").prop("disabled", true);
+    $("#inscricaoEstadual_input").prop("disabled", true);
+    $("#cnae_input").prop("disabled", true);
+
+    $("#p-incubada").click(function (){
+        $("#cpf_input").prop("disabled", false);
+        $("#cnpj_input").prop("disabled", true);
+        $("#inscricaoEstadual_input").prop("disabled", true);
+        $("#cnae_input").prop("disabled", true);
+
+        $("#fase").val(0);
+        $("#incubada").val(0);
+        $("#p-incubada").val(200);
+    });
+    $("#incubada").click(function (){
+        $("#cpf_input").prop("disabled", true);
+        $("#cnpj_input").prop("disabled", false);
+        $("#inscricaoEstadual_input").prop("disabled", false);
+        $("#cnae_input").prop("disabled", false);
+
+        $("#fase").val(0);
+        $("#p-incubada").val(0);
+        $("#incubada").val(200);
+    });
+    $("#fase").click(function (){
+        $("#p-incubada").val(0);
+        $("#incubada").val(0);
+        $("#fase").val(200);
+
+        $("#cpf_input").prop("disabled", true);
+        $("#cnpj_input").prop("disabled", true);
+        $("#inscricaoEstadual_input").prop("disabled", true);
+        $("#cnae_input").prop("disabled", true);
+    });
+});
+/*
+    $('html,body').animate({scrollTop: $(".segunda_sessao").offset().top},'slow');
+*/
---
+++ src/scripts/mascaras-cadastro-empreendimento.js
@@ -0,0 +1,29 @@
+var cpf_input = document.getElementById("cpf_input");
+var maskCPF = {
+    mask: '000.000.000-00'
+};
+var maskCPF_apply = IMask(cpf_input,maskCPF);
+
+var cnpj_input = document.getElementById("cnpj_input");
+var maskCNPJ = {
+    mask: '00.000.000/0000-00'
+};
+var maskCNPJ_apply = IMask(cnpj_input,maskCNPJ);
+
+var cep_input = document.getElementById("cep_input");
+var maskCEP = {
+    mask: '00000-000'
+};
+var maskCEP_apply = IMask(cep_input,maskCEP);
+
+var celular_input = document.getElementById("celular_input");
+var maskCEL = {
+    mask: '(00) 00000-0000'
+};
+var maskCEL_apply = IMask(celular_input,maskCEL);
+
+var telefone_input = document.getElementById("telefone");
+var maskTEL = {
+    mask: '(00) 0000-0000'
+};
+var maskTEL_apply = IMask(telefone_input,maskTEL);
