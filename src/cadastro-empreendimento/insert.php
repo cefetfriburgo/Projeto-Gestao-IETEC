c1c99a01ef109545976224d5fb372e4c385493f3
0z <rodrigonwl.a@gmail.com>
2021-12-09 16:02:54 -0300

    mascaras JS

--- src/cadastro-empreendimento/insert.php
+++ src/cadastro-empreendimento/insert.php
@@ -1,67 +1,67 @@
 
-        <?php
-        include "conexao.php";
-        
-        //$fase= $_POST['fase_incubacao'];
-        $data= filter_var($_POST['data'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
-        $Nomefan= filter_var($_POST['sigla-marca'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
-        $razaosocial = filter_var($_POST['razaoSocial-nome'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
-        //$cpf = $_POST['cpf']; 
-        //$cnpj = $_POST['cnpj']; 
-        //$cnae = $_POST['cnae'];
-        //$inscricaoestadual = $_POST['inscricaoEstadual'];
-        $endereco =filter_var($_POST ['endereco'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        $cep = filter_var($_POST['cep'],FILTER_SANITIZE_ADD_SLASHES,FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        $numero = filter_var($_POST['numero'], FILTER_SANITIZE_NUMBER_INT); //FILTER_SANITIZE_NUMBER_INT
-        $complemento = filter_var($_POST['complemento'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        $bairro = filter_var($_POST['bairro'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        $municipio = filter_var($_POST['municipio'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        $celular1 = filter_var($_POST['celphone'],FILTER_SANITIZE_NUMBER_INT) ;
-        $phone1 = filter_var($_POST['telephone'],FILTER_SANITIZE_NUMBER_INT);
-        $email = filter_var($_POST['e-mail'], FILTER_VALIDATE_EMAIL) ; //FILTER_VALIDATE_EMAIL
-        $website = filter_var($_POST['website'], FILTER_SANITIZE_URL); // FILTER_SANITIZE_URL
-        $logoEmpresa = $_POST['logoEmpresa'];
-        $comentario = filter_var($_POST['comentario'],FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
-        
-        $celular = preg_replace("/[^0-9]/", "", $celular1);
-        $phone = preg_replace("/[^0-9]/", "", $phone1);
-        
-        
-        $insert_endereco = "insert into endereco (endereco,cep,numero,bairro,municipio,complemento) 
-                  values ('$endereco','$cep','$numero','$bairro','$municipio','$complemento')";
+<?php
+include "conexao.php";
 
-        $insert_empresa= "insert into empresa ( Endereco_id, nome_fantasia,data_incubacao,razao_social, inscricao_estadual, cnae, comentario)
-        values( (SELECT MAX(id) FROM endereco), '$Nomefan','$data',' $razaosocial ','0123456789', '1234567', '$comentario')";
-          
+//$fase= $_POST['fase_incubacao'];
+$data= filter_var($_POST['data'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
+$Nomefan= filter_var($_POST['sigla-marca'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
+$razaosocial = filter_var($_POST['razaoSocial-nome'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING);
+//$cpf = $_POST['cpf']; 
+//$cnpj = $_POST['cnpj']; 
+//$cnae = $_POST['cnae'];
+//$inscricaoestadual = $_POST['inscricaoEstadual'];
+$endereco =filter_var($_POST ['endereco'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
+$cep = filter_var($_POST['cep'],FILTER_SANITIZE_ADD_SLASHES,FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
+$numero = filter_var($_POST['numero'], FILTER_SANITIZE_NUMBER_INT); //FILTER_SANITIZE_NUMBER_INT
+$complemento = filter_var($_POST['complemento'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
+$bairro = filter_var($_POST['bairro'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
+$municipio = filter_var($_POST['municipio'], FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
+$celular1 = filter_var($_POST['celphone'],FILTER_SANITIZE_NUMBER_INT) ;
+$phone1 = filter_var($_POST['telephone'],FILTER_SANITIZE_NUMBER_INT);
+$email = filter_var($_POST['e-mail'], FILTER_VALIDATE_EMAIL) ; //FILTER_VALIDATE_EMAIL
+$website = filter_var($_POST['website'], FILTER_SANITIZE_URL); // FILTER_SANITIZE_URL
+$logoEmpresa = $_POST['logoEmpresa'];
+$comentario = filter_var($_POST['comentario'],FILTER_SANITIZE_ADD_SLASHES, FILTER_SANITIZE_STRING); //FILTER_SANITIZE_ADD_SLASHES FILTER_SANITIZE_STRING
 
-        $insert_contato= "insert into contato_empresa ( celular, telefone, email, website, Empresa_id)
-        values( '$celular','$phone', '$email', '$website', (SELECT MAX(id) FROM empresa))";
-        
-        mysqli_query($conexao, $insert_endereco);
-        mysqli_query($conexao, $insert_empresa);
-        mysqli_query($conexao, $insert_contato); 
-        
-
-         
-        // Exibe o erro na consulta
-         /*if ($resultado == false){
-                print_r( $full_query );
-                echo mysqli_error($conexao);
-
-	        die();
-	}*/
+$celular = preg_replace("/[^0-9]/", "", $celular1);
+$phone = preg_replace("/[^0-9]/", "", $phone1);
 
 
-        $LinhasAfetadas = mysqli_affected_rows($conexao);
-        if($LinhasAfetadas==1)
-        {
-                echo("Empresa cadastrada com sucesso");
-        }
-        else
-        {
-                echo("ERRO, empresa não foi cadastrada");
-        }       
+$insert_endereco = "insert into endereco (endereco,cep,numero,bairro,municipio,complemento) 
+        values ('$endereco','$cep','$numero','$bairro','$municipio','$complemento')";
 
-              
+$insert_empresa= "insert into empresa ( Endereco_id, nome_fantasia,data_incubacao,razao_social, inscricao_estadual, cnae, comentario)
+values( (SELECT MAX(id) FROM endereco), '$Nomefan','$data',' $razaosocial ','0123456789', '1234567', '$comentario')";
+
+
+$insert_contato= "insert into contato_empresa ( celular, telefone, email, website, Empresa_id)
+values( '$celular','$phone', '$email', '$website', (SELECT MAX(id) FROM empresa))";
+
+mysqli_query($conexao, $insert_endereco);
+mysqli_query($conexao, $insert_empresa);
+mysqli_query($conexao, $insert_contato); 
+
+
+
+// Exibe o erro na consulta
+/*if ($resultado == false){
+print_r( $full_query );
+echo mysqli_error($conexao);
+
+die();
+}*/
+
+
+$LinhasAfetadas = mysqli_affected_rows($conexao);
+if($LinhasAfetadas==1)
+{
+echo("Empresa cadastrada com sucesso");
+}
+else
+{
+echo("ERRO, empresa não foi cadastrada");
+}       
+
+
 
 ?>
--- src/scripts/mascaras-cadastro-empreendimento.js
+++ src/scripts/mascaras-cadastro-empreendimento.js
@@ -1,31 +1,42 @@
 
-    var cpf_input = document.getElementById("cpf_input");
-    var maskCPF = {
-        mask: '000.000.000-00'
-    };
-    var maskCPF_apply = IMask(cpf_input,maskCPF);
-    
-    var cnpj_input = document.getElementById("cnpj_input");
-    var maskCNPJ = {
-        mask: '00.000.000/0000-00'
-    };
-    var maskCNPJ_apply = IMask(cnpj_input,maskCNPJ);
-    
-    var cep_input = document.getElementById("cep_input");
-    var maskCEP = {
-        mask: '00000-000'
-    };
-    var maskCEP_apply = IMask(cep_input,maskCEP);
-    
-    var celular_input = document.getElementById("celular_input");
-    var maskCEL = {
-        mask: '(00) 00000-0000'
-    };
-    var maskCEL_apply = IMask(celular_input,maskCEL);
-    
-    var telefone_input = document.getElementById("telefone");
-    var maskTEL = {
-        mask: '(00) 0000-0000'
-    };
-    var maskTEL_apply = IMask(telefone_input,maskTEL);
-    
+var cpf_input = document.getElementById("cpf_input");
+var maskCPF = {
+    mask: '000.000.000-00'
+};
+var maskCPF_apply = IMask(cpf_input,maskCPF);
+
+var cnpj_input = document.getElementById("cnpj_input");
+var maskCNPJ = {
+    mask: '00.000.000/0000-00'
+};
+var maskCNPJ_apply = IMask(cnpj_input,maskCNPJ);
+
+var cep_input = document.getElementById("cep_input");
+var maskCEP = {
+    mask: '00000-000'
+};
+var maskCEP_apply = IMask(cep_input,maskCEP);
+
+var celular_input = document.getElementById("celular_input");
+var maskCEL = {
+    mask: '(00) 00000-0000'
+};
+var maskCEL_apply = IMask(celular_input,maskCEL);
+
+var telefone_input = document.getElementById("telefone");
+var maskTEL = {
+    mask: '(00) 0000-0000'
+};
+var maskTEL_apply = IMask(telefone_input,maskTEL);
+
+var cnae_input = document.getElementById("cnae_input");
+var maskCNAE = {
+    mask: '0000-0/00'
+};
+var maskCNAE_apply = IMask(cnae_input,maskCNAE);
+
+var inscricaoEstadual_input = document.getElementById("inscricaoEstadual_input");
+var maskIE = {
+    mask: '000.000.000'
+};
+var maskIE_apply = IMask(inscricaoEstadual_input,maskIE);
