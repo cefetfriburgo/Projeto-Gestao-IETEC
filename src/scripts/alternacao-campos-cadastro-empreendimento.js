dfa85e3b4605a407f4a9ae329b0d0139ba1c9716
0z <rodrigonwl.a@gmail.com>
2021-12-15 17:50:25 -0300

    atualizacao de campos

--- src/scripts/alternacao-campos-cadastro-empreendimento.js
+++ src/scripts/alternacao-campos-cadastro-empreendimento.js
@@ -7,23 +7,22 @@
 
         $("#p-incubada").click(function (){
             $("#cpf_input").prop("disabled", false);
+            $('#cpf_input').removeAttr("disabled"); // Isso resolve o problema do Chromium
+
             $("#cnpj_input").prop("disabled", true);
             $("#inscricaoEstadual_input").prop("disabled", true);
             $("#cnae_input").prop("disabled", true);
-
-            // Isso dah erro ...
 
             $("#fase").val(0);
             $("#incubada").val(0);
             $("#p-incubada").val('p');
         });
         $("#incubada").click(function (){
-            $("#cpf_input").prop("disabled", true);
-            $("#cnpj_input").prop("disabled", false);
-            $("#inscricaoEstadual_input").prop("disabled", false);
-            $("#cnae_input").prop("disabled", false);
+            $('#cpf_input').attr("disabled");
 
-            // Isso dah erro
+            $('#cnpj_input').removeAttr("disabled");
+            $('#inscricaoEstadual_input').removeAttr("disabled");
+            $('#cnae_input').removeAttr("disabled");
 
             $("#fase").val(0);
             $("#p-incubada").val(0);
